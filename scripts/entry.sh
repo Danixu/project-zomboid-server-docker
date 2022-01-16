ee272d974b72bad78e9aaa31be2b5fa2ba39feeb
Daniel Carrasco Marín <danielmadrid19@gmail.com>
2022-01-16 23:28:39 +0100

    Removed quotes from environment variables and fixed Java arguments

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -9,6 +9,19 @@ cd ${STEAMAPPDIR}
 ######################################
 ARGS=""
 
+# Set the server memory. Units are accepted (1024m=1Gig, 2048m=2Gig, 4096m=4Gig): Example: 1024m
+if [ -n "${MEMORY}" ]; then
+  ARGS="${ARGS} -Xmx${MEMORY} -Xms${MEMORY}" 
+fi
+
+# Option to perform a Soft Reset
+if [ "${SOFTRESET}" == "1" ] || [ "${SOFTRESET,,}" == "true" ]; then
+  ARGS="${ARGS} -Dsoftreset" 
+fi
+
+# End of Java arguments
+ARGS="${ARGS} -- "
+
 # Disables Steam integration on server.
 # - Default: Enabled
 if [ "${NOSTEAM}" == "1" ] || [ "${NOSTEAM,,}" == "true" ]; then
@@ -18,14 +31,14 @@ fi
 # Sets the path for the game data cache dir.
 # - Default: ~/Zomboid
 # - Example: /server/Zomboid/data
-if [ "${CACHEDIR}" != "" ]; then
+if [ -n "${CACHEDIR}" ]; then
   ARGS="${ARGS} -cachedir=${CACHEDIR}"
 fi
 
-# Option to control where mods are loaded from. Any of the 3 keywords may be left out and may appear in any order.
+# Option to control where mods are loaded from and the order. Any of the 3 keywords may be left out and may appear in any order.
 # - Default: workshop,steam,mods
 # - Example: mods,steam
-if [ "${MODFOLDERS}" != "" ]; then
+if [ -n "${MODFOLDERS}" ]; then
   ARGS="${ARGS} -modfolders ${MODFOLDERS}"
 fi
 
@@ -39,12 +52,12 @@ fi
 # This option is mandatory the first startup or will be asked in console and startup will fail.
 # Once is launched and data is created, then can be removed without problem.
 # Is recommended to remove it, because the server logs the arguments in clear text, so Admin password will be sent to log in every startup.
-if [ "${ADMINPASSWORD}" != "" ]; then
+if [ -n "${ADMINPASSWORD}" ]; then
   ARGS="${ARGS} -adminpassword ${ADMINPASSWORD}"
 fi
 
 # You can choose a different servername by using this option when starting the server.
-if [ "${SERVERNAME}" != "" ]; then
+if [ -n "${SERVERNAME}" ]; then
   ARGS="${ARGS} -servername ${SERVERNAME}"
 else
   # If not servername is set, use the default name in the next step
@@ -52,7 +65,7 @@ else
 fi
 
 # If preset is set, then the config file is generated when it doesn't exists or SERVERPRESETREPLACE is set to True.
-if [ "${SERVERPRESET}" != "" ]; then
+if [ -n "${SERVERPRESET}" ]; then
   # If preset file doesn't exists then show an error and exit
   if [ ! -f "${STEAMAPPDIR}/media/lua/shared/Sandbox/${SERVERPRESET}.lua" ]; then
     echo "*** ERROR: the preset ${SERVERPRESET} doesn't exists. Please fix the configuration before start the server ***"
@@ -72,39 +85,29 @@ if [ "${SERVERPRESET}" != "" ]; then
 fi
 
 # Option to handle multiple network cards. Example: 127.0.0.1
-if [ "${IP}" != "" ]; then
+if [ -n "${IP}" ]; then
   ARGS="${IP} -ip ${IP}"
 fi
 
 # Set the DefaultPort for the server. Example: 16261
-if [ "${PORT}" != "" ]; then
+if [ -n "${PORT}" ]; then
   ARGS="${ARGS} -port ${PORT}" 
 fi
 
 # Option to enable/disable VAC on Steam servers. On the server command-line use -steamvac true/false. In the server's INI file, use STEAMVAC=true/false.
-if [ "${STEAMVAC}" != "" ]; then
-  ARGS="${ARGS} -steamvac ${STEAMVAC}"
+if [ -n "${STEAMVAC}" ]; then
+  ARGS="${ARGS} -steamvac ${STEAMVAC,,}"
 fi
 
 # Steam servers require two additional ports to function (I'm guessing they are both UDP ports, but you may need TCP as well).
 # These are in addition to the DefaultPort= setting. These can be specified in two ways:
 #  - In the server's INI file as SteamPort1= and SteamPort2=.
 #  - Using STEAMPORT1 and STEAMPORT2 variables.
-if [ "${STEAMPORT1}" != "" ]; then
+if [ -n "${STEAMPORT1}" ]; then
   ARGS="${ARGS} -steamport1 ${STEAMPORT1}"
 fi
-if [ "${STEAMPORT2}" != "" ]; then
+if [ -n "${STEAMPORT2}" ]; then
   ARGS="${ARGS} -steamport2 ${STEAMPORT1}"
-fi
-
-# Set the server memory. Units are accepted (1024m=1Gig, 2048m=2Gig, 4096m=4Gig): Example: 1024m
-if [ "${MEMORY}" != "" ]; then
-  ARGS="${ARGS} -Xmx${MEMORY} -Xms${MEMORY}" 
-fi
-
-# Option to perform a Soft Reset
-if [ "${SOFTRESET}" == "1" ] || [ "${SOFTRESET,,}" == "true" ]; then
-  ARGS="${ARGS} -Dsoftreset" 
 fi
 
 # Fix to a bug in start-server.sh that causes to no preload a library:
